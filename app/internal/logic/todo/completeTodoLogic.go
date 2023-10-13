8d71212e1c91123345ae83c1ebd63df8e6e71d97
muskonu <22050918@hdu.edu.cn>
2023-10-13 13:41:11 +0800

    fix: Fix null pointer error

--- app/internal/logic/todo/completeTodoLogic.go
+++ app/internal/logic/todo/completeTodoLogic.go
@@ -5,7 +5,6 @@ import (
 	"TodoList/app/internal/types"
 	"TodoList/common/ctxdata"
 	"TodoList/common/errorz"
-	"TodoList/model"
 	"context"
 	"github.com/pkg/errors"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -31,7 +30,7 @@ func (l *CompleteTodoLogic) CompleteTodo(req *types.CompleteTodoRequest) error {
 	err := l.svcCtx.TodoListModel.Transaction(l.ctx, func(db *gorm.DB) error {
 		//更改数据库状态
 		old, err := l.svcCtx.TodoListModel.FindOne(l.ctx, req.Id)
-		if err != nil && err != model.ErrNotFound {
+		if err != nil {
 			return errors.Wrapf(errorz.NewErrCode(errorz.DB_ERROR), "todo: %v, err: %+v", req.Id, err)
 		}
 		err = l.svcCtx.TodoListModel.ChangeComplete(l.ctx, db, old)
@@ -40,7 +39,7 @@ func (l *CompleteTodoLogic) CompleteTodo(req *types.CompleteTodoRequest) error {
 		}
 		//更改提醒状态
 		user, err := l.svcCtx.UserModel.FindOne(l.ctx, id)
-		if err != nil && err != model.ErrNotFound {
+		if err != nil {
 			return errors.Wrapf(errorz.NewErrCode(errorz.DB_ERROR), "user: %v, err: %+v", id, err)
 		}
 		if old.IsCompleted {
